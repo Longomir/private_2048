865a4492990dd177415b8dee954a6b88fb4ad95d
Martin Zangger <martin.zangger@it-processing.ch>
2017-04-20 15:29:01 +0200

    initial commit

---
+++ src/application/GameController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameController {
+
+	
+	
+	@FXML protected void moveField(ActionEvent e){
+		System.out.println("moved");
+	}
+	
+}
